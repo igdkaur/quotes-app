832e01d0f9a308cd22191e8d3b9f35db8bdd8193
igdkaur <igdkaur@gmail.com>
2022-01-26 19:13:43 +0530

    landing page redirect

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route,Switch } from 'react-router-dom'
 import AllQuotes from './pages/AllQuotes';
 import QuoteDetail from './pages/QuoteDetail';
 import  NewQuotes from './pages/NewQuotes';
+import { Redirect } from 'react-router-dom';
 
 
 function App() {
   return (
     <Switch>
+       <Route path = '/' exact> <Redirect to ='/quotes' /></Route> 
       <Route path = '/quotes' exact> <AllQuotes /></Route> 
       <Route path = '/quotes/:quoteId'> <QuoteDetail /> </Route>
       <Route path = '/new-quote'> <NewQuotes /> </Route>
