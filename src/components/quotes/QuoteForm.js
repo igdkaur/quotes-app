828518affb79f48db94258662800e2197612074e
igdkaur <igdkaur@gmail.com>
2022-01-26 21:29:12 +0530

    prompt user

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,10 +1,12 @@
-import { useRef } from 'react';
+import { useRef,useState,Fragment } from 'react';
+import { Prompt } from 'react-router-dom'; 
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
+  const [isEntering, setIsEntering] = useState(false)
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -18,10 +20,20 @@ const QuoteForm = (props) => {
 
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
+  const handleOnFocus = () => {
+    setIsEntering(true);
+  }
 
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  }
+  
+ 
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+    <Fragment>
+      <Prompt when = {isEntering} message={() => 'Leave for sure?'} / > 
+      <Card>
+      <form onFocus={handleOnFocus} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +49,12 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button className='btn' onClick={finishEnteringHandler}>Add Quote</button>
         </div>
       </form>
     </Card>
+    </Fragment>
+    
   );
 };
 
