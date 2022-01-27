13905e2c179c25c0334c158f53a81ef1fe1797ee
igdkaur <igdkaur@gmail.com>
2022-01-27 15:13:29 +0530

    fetch single quote by id

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,7 +1,11 @@
-import { Fragment } from "react";
+import { Fragment,useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
 
 const DUMMY_QUOTES = [
   {
@@ -22,18 +26,50 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
-  const params = useParams();
+  const params = useParams(); /// get id from here
   const match = useRouteMatch();
-  console.log(match); 
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
-  if (!quote) {
-    return <p> No quotes found!</p>;
+  const {quoteId} = params
+  
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+
+  if(status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner/>
+      </div>
+    )
   }
+  if(error) {
+    return (
+      <div className="centered focused">
+        {error}
+      </div>
+    )
+  }
+  
+  if(!loadedQuote.text) {
+    return<p>No quote found!</p>
+  }
+
+  // const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  // if (!quote) {
+  //   return <p> No quotes found!</p>; // loaded quote ka text check -- do 
+  // }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={`/quotes/${params.quoteId}`} exact>
       <div className="centered">
         <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}> 
